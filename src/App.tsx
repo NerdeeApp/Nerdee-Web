b25e60e0a68554485654b3f7a3b10bb5108e9836
Rhys Woolcott <rhyswoolcott@gmail.com>
2021-04-04 20:15:15 +0100

    fixed some images

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import './style/main.scss';
 import './style/global.scss';
 import './style/preloader.scss';
 import Blobs from './images/SVG/Blobs.svg';
+// import BlobsMobile from './images/SVG/BlobsMobile.svg';
 // import { Navbar, Preloader } from './components';
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={Blobs} alt='Blobs' id='#blobs' />
-			</header>
+			<header className='App-header' />
+			<div id='backgrd' />
 		</div>
 	);
 }
