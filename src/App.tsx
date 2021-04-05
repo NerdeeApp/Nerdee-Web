c11df6cd20347ccd643b708160743a1e6c24a921
Rhys Woolcott <rhyswoolcott@gmail.com>
2021-04-05 18:53:34 +0100

    Added some responsive functionality

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,24 @@ function App() {
 	return (
 		<div className='App'>
 			<header className='App-header' />
-			<div id='backgrd' />
+			<div id='backgrd'>
+				<h1>
+					NERDEE<br />
+					<span id='subtext'>
+						<span>D</span>
+						<span>A</span>
+						<span>T</span>
+						<span>E</span>
+						<span> </span>
+						<span>A</span>
+						<span> </span>
+						<span>T</span>
+						<span>E</span>
+						<span>C</span>
+						<span>H</span>
+					</span>
+				</h1>
+			</div>
 		</div>
 	);
 }
