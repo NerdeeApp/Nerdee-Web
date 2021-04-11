f2ac55022b9d1486a522c0063819b77d26cecefa
Rhys Woolcott <rhyswoolcott@gmail.com>
2021-04-11 17:45:27 +0100

    more shit added

--- src/pages/page/Home.tsx
+++ src/pages/page/Home.tsx
@@ -39,26 +39,35 @@ export class Home extends Component {
 							<span>H</span>
 						</span>
 					</h1>
-					<button id='joinNow' onClick={test}>
+					<button id='joinNow' onClick={alrt}>
 						JOIN NOW
 					</button>
 
-					<img src='/images/icons/down.svg' alt='' id='scroll__down' />
+					<img src='/images/icons/down.svg' alt='' id='scroll__down' onClick={scroll__down} />
 				</div>
 
 				<div id='about'>
 					<h1 id='title'>ABOUT US</h1>
 					<p id='info'>
-						<b>Our Goals</b>
+						<span id="title__bar"><h2><b>FAQ</b></h2></span>
+						<b>Our Goals.</b>
 						<br />
 						Here at Nerdee, we aim for our clients to enjoy life with someone who has similar thoughts,
-						similar interests and similar experiences.
+						similar interests and similar experiences without the extras that are unrelated to finding a
+						good date.
 						<br />
 						<br />
 						<b>What about my data? What will you use it for?</b>
 						<br />
 						We only use data for finding you your best partner, <b>ALL</b> data is end to end encrypted
 						using the AES256 algorithm so none of your data is accessable without your permission.
+						<br />
+						<br />
+						<b>Why was this app started?</b>
+						<br />
+						We started this app because of frustration in current dating apps such as tinder and okcupid
+						matching people based on "Star sign compatability" and other methods that are completely
+						unrelated to finding someone that you, the user will love and appreciate.
 					</p>
 				</div>
 
@@ -68,6 +77,12 @@ export class Home extends Component {
 	}
 }
 
-let test = () => {
+let alrt = () => {
 	alert('This site is currently in development so is not live. Please return soon to test');
+};
+
+let scroll__down = () => {
+	let about = document.getElementById("about");
+
+	about?.scrollIntoView({ behavior: "smooth", block: "start" })
 };
