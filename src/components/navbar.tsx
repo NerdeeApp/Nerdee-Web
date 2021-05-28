2bbd01fd081decb9ff0c8f5093d785a93fbfcf85
Rhys Woolcott <rhyswoolcott@gmail.com>
2021-05-28 20:33:24 +0100

    added responsive to navbar. Logged-in alternative coming soon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,8 @@
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 export const Navbar: FC<any> = () => {
+	const [ sideNavState, setSideNavState ] = useState(false);
 	const history = useHistory();
 
 	const loginSend = () => {
@@ -12,24 +13,52 @@ export const Navbar: FC<any> = () => {
 		return (window.location.href = '/register');
 	};
 
-	return (
-		<nav>
-			<a href='/' id='nav__logo'>
-				<img src='/images/SVG/logo.svg' alt='' />
-			</a>
+	const slideIn = () => {
+		if (sideNavState) return setSideNavState(false);
 
-			<div className='right'>
-				<div className='default'>
-					<button className='nav__notbutton' id='nav__register' onClick={registerSend} tabIndex={0}>
-						Register
-						<div className='hover_border__bottom' />
-					</button>
-					<button className='nav__notbutton' id='nav__login' onClick={loginSend} tabIndex={0}>
-						Login
-						<div className='hover_border__bottom' />
-					</button>
+		setSideNavState(true);
+	};
+
+	return (
+		<div>
+			<nav>
+				<a href='/' id='nav__logo'>
+					<img src='/images/SVG/logo.svg' alt='' />
+				</a>
+
+				<div className='right'>
+					<div className='default'>
+						<button className='nav__notbutton' id='nav__register' onClick={registerSend} tabIndex={0}>
+							Register
+							<div className='hover_border__bottom' />
+						</button>
+						<button className='nav__notbutton' id='nav__login' onClick={loginSend} tabIndex={0}>
+							Login
+							<div className='hover_border__bottom' />
+						</button>
+					</div>
 				</div>
+
+				<div className='burger' onClick={slideIn}>
+					<div className='burger_bar' />
+					<div className='burger_bar' />
+					<div className='burger_bar' />
+				</div>
+			</nav>
+
+			<div className='side_nav' id={sideNavState ? '' : 'hide'}>
+				<ul>
+					<li>
+						<a href='/'>Home</a>
+					</li>
+					<li>
+						<a href='/login'>Login</a>
+					</li>
+					<li>
+						<a href='/register'>Register</a>
+					</li>
+				</ul>
 			</div>
-		</nav>
+		</div>
 	);
 };
