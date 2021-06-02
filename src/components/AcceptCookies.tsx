ce1c2eb6da384d34342e18b1bc748c00c6239431
Rhys Woolcott <rhyswoolcott@gmail.com>
2021-06-02 17:38:52 +0100

    Fixed some wrapping and other issues in dashboard and cookie banner

--- src/components/AcceptCookies.tsx
+++ src/components/AcceptCookies.tsx
@@ -7,16 +7,19 @@ export const AcceptCookies: FC<any> = () => {
 
 		Cookie.set('AcceptedCookies', true, { maxAge: 365 * 24 * 60 * 60 });
 
-		document.getElementsByClassName('CookieBanner')[0].classList.toggle('hidden');
+		document.getElementsByClassName('cookieHolder')[0].classList.toggle('hidden');
 	};
 
 	return (
-		<div className='CookieBanner'>
-			<p>
-				As required by law, our site notifies users of cookies used to improve your experience. Following GDPR
-				guidelines, we will request for you to accept these cookies to make the site work properly.
-			</p>
-			<button onClick={HideCookie}>Accept</button>
+		<div className='cookieHolder'>
+			<div className='background' />
+			<div className='CookieBanner'>
+				<p>
+					As required by law, our site notifies users of cookies used to improve your experience. Following
+					GDPR guidelines, we will request for you to accept these cookies to make the site work properly.
+				</p>
+				<button onClick={HideCookie}>Accept</button>
+			</div>
 		</div>
 	);
 };
