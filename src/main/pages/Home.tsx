281c7555ce9bf0c8769e88a7e97efee920ac37df
Rhys Woolcott <rhyswoolcott@gmail.com>
2021-07-16 19:06:18 +0100

    Added some more buttons and stuff

--- src/main/pages/Home.tsx
+++ src/main/pages/Home.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import useScrollSnap from "react-use-scroll-snap";
+import { Button } from "../components";
 
 const Home = () => {
 	let [small, setSmall] = useState(false);
@@ -40,11 +41,29 @@ const Home = () => {
 			<div className='container'>
 				<section>
 					<img src='/images/PNG/title.png' alt='logo text' id='title' />
+
+					<div id='buttonContainer'>
+						<Button id='apple' onClick={() => alert("Not available yet")}>
+							Apple
+						</Button>
+						<Button id='playstore' onClick={() => alert("Not available yet")}>
+							Google Play
+						</Button>
+					</div>
+
 					<div id='imageOverlay'></div>
 					<img src='/images/PNG/splash.jpg' alt='' id='headerImage' />
 				</section>
 				<section>
-					<h1>Content 1</h1>
+					<svg
+						preserveAspectRatio='none'
+						width='100%'
+						height='5%'
+						viewBox='0 0 100 5'
+					>
+						<path d='M 0 5 L 100 5 L 100 0 L 0 5' fill='#151515'></path>
+					</svg>
+					<h1 id='aboutUs'>About Us</h1>
 				</section>
 			</div>
 		</>
